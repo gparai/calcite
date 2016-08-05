gparai/calcite

commit 69839c3798c8221081d80edaaca27a310ce9005e

[CALCITE-1327] In nested windowed aggregates, check that PARTITION BY columns are in GROUP BY (Gautam Parai)
Also fixes [CALCITE-1340] Invalid errors for windowed aggregates.

Discriminate aggregate functions from mere window functions. Don't
declare "SELECT RANK()" to be an aggregating query; even if it's
missing OVER, RANK is only a window function, not an aggregate function.

Close apache/calcite#258

core/src/main/java/org/apache/calcite/sql/SqlAggFunction.java
@@ -78,11 +78,11 @@ public abstract class SqlAggFunction extends SqlFunction implements Context {
       SqlOperandTypeChecker operandTypeChecker,
       SqlFunctionCategory funcType,
       boolean requiresOrder,
-      boolean requireOver) {
+      boolean requiresOver) {
     super(name, sqlIdentifier, kind, returnTypeInference, operandTypeInference,
         operandTypeChecker, null, funcType);
     this.requiresOrder = requiresOrder;
-    this.requiresOver =  requireOver;
+    this.requiresOver = requiresOver;
   }
 
   //~ Methods ----------------------------------------------------------------
@@ -112,15 +112,7 @@ public abstract class SqlAggFunction extends SqlFunction implements Context {
     return requiresOrder;
   }
 
-  /** Returns whether this is a window function that requires an OVER clause.
-   *
-   * <p>For example, {@code RANK} and {@code DENSE_RANK} require an OVER clause;
-   * {@code SUM} does not (it can be used as a non-window aggregate function).
-   *
-   * @see #allowsFraming()
-   * @see #requiresOrder()
-   */
-  public final boolean requiresOver() {
+  @Override public final boolean requiresOver() {
     return requiresOver;
   }
 

core/src/main/java/org/apache/calcite/sql/SqlOperator.java
@@ -752,6 +752,23 @@ public abstract class SqlOperator {
     return false;
   }
 
+  /** Returns whether this is a window function that requires an OVER clause.
+   *
+   * <p>For example, returns true for {@code RANK}, {@code DENSE_RANK} and
+   * other ranking functions; returns false for {@code SUM}, {@code COUNT},
+   * {@code MIN}, {@code MAX}, {@code AVG} (they can be used as non-window
+   * aggregate functions).
+   *
+   * <p>If {@code requiresOver} returns true, then {@link #isAggregator()} must
+   * also return true.
+   *
+   * @see #allowsFraming()
+   * @see #requiresOrder()
+   */
+  public boolean requiresOver() {
+    return false;
+  }
+
   /**
    * Returns whether this is a window function that requires ordering.
    *

core/src/main/java/org/apache/calcite/sql/validate/AggChecker.java
@@ -23,6 +23,7 @@ import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlNodeList;
 import org.apache.calcite.sql.SqlSelect;
 import org.apache.calcite.sql.SqlUtil;
+import org.apache.calcite.sql.SqlWindow;
 import org.apache.calcite.sql.util.SqlBasicVisitor;
 import org.apache.calcite.util.Litmus;
 
@@ -79,13 +80,9 @@ class AggChecker extends SqlBasicVisitor<Void> {
   }
 
   public Void visit(SqlIdentifier id) {
-    if (isGroupExpr(id)) {
+    if (isGroupExpr(id) || id.isStar()) {
+      // Star may validly occur in "SELECT COUNT(*) OVER w"
       return null;
-    }
-
-    // If it '*' or 'foo.*'?
-    if (id.isStar()) {
-      assert false : "star should have been expanded";
     }
 
     // Is it a call to a parentheses-free function?
@@ -150,10 +147,20 @@ class AggChecker extends SqlBasicVisitor<Void> {
       return null;
     }
     // Visit the operand in window function
-    if (call.getOperator().getKind() == SqlKind.OVER) {
-      SqlCall windowFunction = call.operand(0);
-      if (windowFunction.getOperandList().size() != 0) {
-        windowFunction.operand(0).accept(this);
+    if (call.getKind() == SqlKind.OVER) {
+      for (SqlNode operand : call.<SqlCall>operand(0).getOperandList()) {
+        operand.accept(this);
+      }
+      // Check the OVER clause
+      final SqlNode over = call.operand(1);
+      if (over instanceof SqlCall) {
+        over.accept(this);
+      } else if (over instanceof SqlIdentifier) {
+        // Check the corresponding SqlWindow in WINDOW clause
+        final SqlWindow window =
+            scope.lookupWindow(((SqlIdentifier) over).getSimple());
+        window.getPartitionList().accept(this);
+        window.getOrderList().accept(this);
       }
     }
     if (isGroupExpr(call)) {

core/src/main/java/org/apache/calcite/sql/validate/AggFinder.java
@@ -100,9 +100,9 @@ class AggFinder extends SqlBasicVisitor<Void> {
   public Void visit(SqlCall call) {
     final SqlOperator operator = call.getOperator();
     // If nested aggregates disallowed or found an aggregate at invalid level
-    if (operator.isAggregator()) {
+    if (operator.isAggregator() && !operator.requiresOver()) {
       if (delegate != null) {
-        return call.getOperator().acceptCall(delegate, call);
+        return operator.acceptCall(delegate, call);
       }
       if (aggregate) {
         throw new Util.FoundOne(call);
@@ -115,8 +115,8 @@ class AggFinder extends SqlBasicVisitor<Void> {
         final List<SqlOperator> list = Lists.newArrayList();
         opTab.lookupOperatorOverloads(sqlFunction.getSqlIdentifier(),
             sqlFunction.getFunctionType(), SqlSyntax.FUNCTION, list);
-        for (SqlOperator sqlOperator : list) {
-          if (sqlOperator.isAggregator()) {
+        for (SqlOperator operator2 : list) {
+          if (operator2.isAggregator() && !operator2.requiresOver()) {
             // If nested aggregates disallowed or found aggregate at invalid level
             if (aggregate) {
               throw new Util.FoundOne(call);

core/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
@@ -34,7 +34,6 @@ import org.apache.calcite.sql.JoinConditionType;
 import org.apache.calcite.sql.JoinType;
 import org.apache.calcite.sql.SqlAccessEnum;
 import org.apache.calcite.sql.SqlAccessType;
-import org.apache.calcite.sql.SqlAggFunction;
 import org.apache.calcite.sql.SqlBasicCall;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlCallBinding;
@@ -2504,7 +2503,28 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
   }
 
   public boolean isAggregate(SqlSelect select) {
-    return getAggregate(select) != null;
+    if (getAggregate(select) != null) {
+      return true;
+    }
+    // Also when nested window aggregates are present
+    for (SqlNode node : select.getSelectList()) {
+      if (node instanceof SqlCall) {
+        SqlCall call = (SqlCall) node;
+        if (call.getOperator().getKind() == SqlKind.OVER
+            && call.getOperandList().size() != 0) {
+          if (call.operand(0) instanceof SqlCall
+              && isNestedAggregateWindow((SqlCall) call.operand(0))) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  protected boolean isNestedAggregateWindow(SqlCall windowFunction) {
+    AggFinder nestedAggFinder = new AggFinder(opTab, false, false, aggFinder);
+    return nestedAggFinder.findAgg(windowFunction) != null;
   }
 
   /** Returns the parse tree node (GROUP BY, HAVING, or an aggregate function
@@ -3327,10 +3347,15 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
     }
   }
 
+  /**
+   * Validates an item in the ORDER BY clause of a SELECT statement.
+   *
+   * @param select Select statement
+   * @param orderItem ORDER BY clause item
+   */
   private void validateOrderItem(SqlSelect select, SqlNode orderItem) {
-    if (SqlUtil.isCallTo(
-        orderItem,
-        SqlStdOperatorTable.DESC)) {
+    switch (orderItem.getKind()) {
+    case DESCENDING:
       validateFeature(RESOURCE.sQLConformance_OrderByDesc(),
           orderItem.getParserPosition());
       validateOrderItem(select,
@@ -3526,15 +3551,6 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
       }
     }
 
-    // Check expanded select list for aggregation.
-    if (selectScope instanceof AggregatingScope) {
-      AggregatingScope aggScope = (AggregatingScope) selectScope;
-      for (SqlNode selectItem : expandedSelectItems) {
-        boolean matches = aggScope.checkAggregateExpr(selectItem, true);
-        Util.discard(matches);
-      }
-    }
-
     // Create the new select list with expanded items.  Pass through
     // the original parser position so that any overall failures can
     // still reference the original input text.
@@ -3567,9 +3583,8 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
    */
   private void validateExpr(SqlNode expr, SqlValidatorScope scope) {
     if (expr instanceof SqlCall) {
-      final SqlCall sqlCall = (SqlCall) expr;
-      if (sqlCall.getOperator().isAggregator()
-          && ((SqlAggFunction) sqlCall.getOperator()).requiresOver()) {
+      final SqlOperator op = ((SqlCall) expr).getOperator();
+      if (op.isAggregator() && op.requiresOver()) {
         throw newValidationError(expr,
             RESOURCE.absentOverClause());
       }

core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -4544,9 +4544,6 @@ public class SqlToRelConverter {
       // Ignore window aggregates and ranking functions (associated with OVER
       // operator). However, do not ignore nested window aggregates.
       if (call.getOperator().getKind() == SqlKind.OVER) {
-        if (call.operand(0).getKind() == SqlKind.RANK) {
-          return null;
-        }
         // Track aggregate nesting levels only within an OVER operator.
         inOver = true;
       }

commit 22942c6742fe81240b30e0740d8e70028639bcfc

[CALCITE-1327] Nested aggregate windowed query fails
Cherry pick changes from commit 69839c3

core/src/main/java/org/apache/calcite/sql/SqlAggFunction.java
@@ -74,11 +74,11 @@ public abstract class SqlAggFunction extends SqlFunction implements Context {
       SqlOperandTypeChecker operandTypeChecker,
       SqlFunctionCategory funcType,
       boolean requiresOrder,
-      boolean requireOver) {
+      boolean requiresOver) {
     super(name, sqlIdentifier, kind, returnTypeInference, operandTypeInference,
         operandTypeChecker, null, funcType);
     this.requiresOrder = requiresOrder;
-    this.requiresOver =  requireOver;
+    this.requiresOver = requiresOver;
   }
 
   //~ Methods ----------------------------------------------------------------
@@ -108,15 +108,7 @@ public abstract class SqlAggFunction extends SqlFunction implements Context {
     return requiresOrder;
   }
 
-  /** Returns whether this is a window function that requires an OVER clause.
-   *
-   * <p>For example, {@code RANK} and {@code DENSE_RANK} require an OVER clause;
-   * {@code SUM} does not (it can be used as a non-window aggregate function).
-   *
-   * @see #allowsFraming()
-   * @see #requiresOrder()
-   */
-  public final boolean requiresOver() {
+  @Override public final boolean requiresOver() {
     return requiresOver;
   }
 }

core/src/main/java/org/apache/calcite/sql/SqlOperator.java
@@ -751,6 +751,23 @@ public abstract class SqlOperator {
     return false;
   }
 
+  /** Returns whether this is a window function that requires an OVER clause.
+   *
+   * <p>For example, returns true for {@code RANK}, {@code DENSE_RANK} and
+   * other ranking functions; returns false for {@code SUM}, {@code COUNT},
+   * {@code MIN}, {@code MAX}, {@code AVG} (they can be used as non-window
+   * aggregate functions).
+   *
+   * <p>If {@code requiresOver} returns true, then {@link #isAggregator()} must
+   * also return true.
+   *
+   * @see #allowsFraming()
+   * @see #requiresOrder()
+   */
+  public boolean requiresOver() {
+    return false;
+  }
+
   /**
    * Returns whether this is a window function that requires ordering.
    *

core/src/main/java/org/apache/calcite/sql/validate/AggChecker.java
@@ -23,6 +23,7 @@ import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlNodeList;
 import org.apache.calcite.sql.SqlSelect;
 import org.apache.calcite.sql.SqlUtil;
+import org.apache.calcite.sql.SqlWindow;
 import org.apache.calcite.sql.util.SqlBasicVisitor;
 import org.apache.calcite.util.Stacks;
 
@@ -79,13 +80,9 @@ class AggChecker extends SqlBasicVisitor<Void> {
   }
 
   public Void visit(SqlIdentifier id) {
-    if (isGroupExpr(id)) {
+    if (isGroupExpr(id) || id.isStar()) {
+      // Star may validly occur in "SELECT COUNT(*) OVER w"
       return null;
-    }
-
-    // If it '*' or 'foo.*'?
-    if (id.isStar()) {
-      assert false : "star should have been expanded";
     }
 
     // Is it a call to a parentheses-free function?
@@ -149,10 +146,20 @@ class AggChecker extends SqlBasicVisitor<Void> {
       return null;
     }
     // Visit the operand in window function
-    if (call.getOperator().getKind() == SqlKind.OVER) {
-      SqlCall windowFunction = (SqlCall) call.operand(0);
-      if (windowFunction.getOperandList().size() != 0) {
-        windowFunction.operand(0).accept(this);
+    if (call.getKind() == SqlKind.OVER) {
+      for (SqlNode operand : call.<SqlCall>operand(0).getOperandList()) {
+        operand.accept(this);
+      }
+      // Check the OVER clause
+      final SqlNode over = call.operand(1);
+      if (over instanceof SqlCall) {
+        over.accept(this);
+      } else if (over instanceof SqlIdentifier) {
+        // Check the corresponding SqlWindow in WINDOW clause
+        final SqlWindow window =
+            scope.lookupWindow(((SqlIdentifier) over).getSimple());
+        window.getPartitionList().accept(this);
+        window.getOrderList().accept(this);
       }
     }
     if (isGroupExpr(call)) {

core/src/main/java/org/apache/calcite/sql/validate/AggFinder.java
@@ -103,26 +103,27 @@ class AggFinder extends SqlBasicVisitor<Void> {
   public Void visit(SqlCall call) {
     final SqlOperator operator = call.getOperator();
     // If nested aggregates disallowed or found an aggregate at invalid level
-    if (operator.isAggregator()) {
+    if (operator.isAggregator() && !operator.requiresOver()) {
       if (delegate != null) {
-        return call.getOperator().acceptCall(delegate, call);
+        return operator.acceptCall(delegate, call);
       }
       if (aggregate) {
         throw new Util.FoundOne(call);
       }
     }
     // User-defined function may not be resolved yet.
-    if (operator instanceof SqlFunction
-        && ((SqlFunction) operator).getFunctionType()
-        == SqlFunctionCategory.USER_DEFINED_FUNCTION) {
-      final List<SqlOperator> list = Lists.newArrayList();
-      opTab.lookupOperatorOverloads(((SqlFunction) operator).getSqlIdentifier(),
-          SqlFunctionCategory.USER_DEFINED_FUNCTION, SqlSyntax.FUNCTION, list);
-      for (SqlOperator sqlOperator : list) {
-        if (sqlOperator.isAggregator()) {
-          // If nested aggregates disallowed or found aggregate at invalid level
-          if (aggregate) {
-            throw new Util.FoundOne(call);
+    if (operator instanceof SqlFunction) {
+      final SqlFunction sqlFunction = (SqlFunction) operator;
+      if (sqlFunction.getFunctionType() == SqlFunctionCategory.USER_DEFINED_FUNCTION) {
+        final List<SqlOperator> list = Lists.newArrayList();
+        opTab.lookupOperatorOverloads(sqlFunction.getSqlIdentifier(),
+            sqlFunction.getFunctionType(), SqlSyntax.FUNCTION, list);
+        for (SqlOperator operator2 : list) {
+          if (operator2.isAggregator() && !operator2.requiresOver()) {
+            // If nested aggregates disallowed or found aggregate at invalid level
+            if (aggregate) {
+              throw new Util.FoundOne(call);
+            }
           }
         }
       }

core/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
@@ -30,7 +30,6 @@ import org.apache.calcite.sql.JoinConditionType;
 import org.apache.calcite.sql.JoinType;
 import org.apache.calcite.sql.SqlAccessEnum;
 import org.apache.calcite.sql.SqlAccessType;
-import org.apache.calcite.sql.SqlAggFunction;
 import org.apache.calcite.sql.SqlBasicCall;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlCallBinding;
@@ -2511,7 +2510,28 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
   }
 
   public boolean isAggregate(SqlSelect select) {
-    return getAggregate(select) != null;
+    if (getAggregate(select) != null) {
+      return true;
+    }
+    // Also when nested window aggregates are present
+    for (SqlNode node : select.getSelectList()) {
+      if (node instanceof SqlCall) {
+        SqlCall call = (SqlCall) node;
+        if (call.getOperator().getKind() == SqlKind.OVER
+            && call.getOperandList().size() != 0) {
+          if (call.operand(0) instanceof SqlCall
+              && isNestedAggregateWindow((SqlCall) call.operand(0))) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  protected boolean isNestedAggregateWindow(SqlCall windowFunction) {
+    AggFinder nestedAggFinder = new AggFinder(opTab, false, false, aggFinder);
+    return nestedAggFinder.findAgg(windowFunction) != null;
   }
 
   /** Returns the parse tree node (GROUP BY, HAVING, or an aggregate function
@@ -3255,10 +3275,15 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
     }
   }
 
+  /**
+   * Validates an item in the ORDER BY clause of a SELECT statement.
+   *
+   * @param select Select statement
+   * @param orderItem ORDER BY clause item
+   */
   private void validateOrderItem(SqlSelect select, SqlNode orderItem) {
-    if (SqlUtil.isCallTo(
-        orderItem,
-        SqlStdOperatorTable.DESC)) {
+    switch (orderItem.getKind()) {
+    case DESCENDING:
       validateFeature(RESOURCE.sQLConformance_OrderByDesc(),
           orderItem.getParserPosition());
       validateOrderItem(select,
@@ -3461,15 +3486,6 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
       }
     }
 
-    // Check expanded select list for aggregation.
-    if (selectScope instanceof AggregatingScope) {
-      AggregatingScope aggScope = (AggregatingScope) selectScope;
-      for (SqlNode selectItem : expandedSelectItems) {
-        boolean matches = aggScope.checkAggregateExpr(selectItem, true);
-        Util.discard(matches);
-      }
-    }
-
     // Create the new select list with expanded items.  Pass through
     // the original parser position so that any overall failures can
     // still reference the original input text.
@@ -3504,9 +3520,8 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
    */
   private void validateExpr(SqlNode expr, SqlValidatorScope scope) {
     if (expr instanceof SqlCall) {
-      final SqlCall sqlCall = (SqlCall) expr;
-      if (sqlCall.getOperator().isAggregator()
-          && ((SqlAggFunction) sqlCall.getOperator()).requiresOver()) {
+      final SqlOperator op = ((SqlCall) expr).getOperator();
+      if (op.isAggregator() && op.requiresOver()) {
         throw newValidationError(expr,
             RESOURCE.absentOverClause());
       }

core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -4428,9 +4428,6 @@ public class SqlToRelConverter {
       // Ignore window aggregates and ranking functions (associated with OVER
       // operator). However, do not ignore nested window aggregates.
       if (call.getOperator().getKind() == SqlKind.OVER) {
-        if (call.operand(0).getKind() == SqlKind.RANK) {
-          return null;
-        }
         // Track aggregate nesting levels only within an OVER operator.
         inOver = true;
       }

core/src/test/java/org/apache/calcite/test/SqlValidatorTest.java
@@ -3806,7 +3806,9 @@ public class SqlValidatorTest extends SqlValidatorTestCase {
 
   /** Test case for
    * <a href="https://issues.apache.org/jira/browse/CALCITE-820">[CALCITE-820]
-   * Validate that window functions have OVER clause</a>. */
+   * Validate that window functions have OVER clause</a>, and
+   * <a href="https://issues.apache.org/jira/browse/CALCITE-1340">[CALCITE-1340]
+   * Window aggregates give invalid errors</a>. */
   @Test public void testWindowFunctionsWithoutOver() {
     winSql(
         "select sum(empno) \n"
@@ -3820,6 +3822,9 @@ public class SqlValidatorTest extends SqlValidatorTestCase {
         + "from emp")
         .fails("OVER clause is necessary for window functions");
 
+    // With [CALCITE-1340], the validator would see RANK without OVER,
+    // mistakenly think this is an aggregating query, and wrongly complain
+    // about the PARTITION BY: "Expression 'DEPTNO' is not being grouped"
     winSql(
         "select cume_dist() over w , ^rank()^\n"
         + "from emp \n"
@@ -4882,7 +4887,7 @@ public class SqlValidatorTest extends SqlValidatorTestCase {
         .fails(
             "Invalid number of arguments to function 'GROUPING'. Was expecting 1 arguments");
     sql("select deptno, grouping(^empno^) from emp group by deptno")
-        .fails("Expression 'EMPNO' is not being grouped");
+        .fails("Argument to GROUPING operator must be a grouped expression");
     sql("select deptno, grouping(^deptno + 1^) from emp group by deptno")
         .fails("Argument to GROUPING operator must be a grouped expression");
     sql("select deptno, grouping(emp.^xxx^) from emp")
@@ -4942,7 +4947,7 @@ public class SqlValidatorTest extends SqlValidatorTestCase {
         .fails(
             "Invalid number of arguments to function 'GROUPING_ID'. Was expecting 1 arguments");
     sql("select deptno, grouping_id(^empno^) from emp group by deptno")
-        .fails("Expression 'EMPNO' is not being grouped");
+        .fails("Argument to GROUPING_ID operator must be a grouped expression");
     sql("select deptno, grouping_id(^deptno + 1^) from emp group by deptno")
         .fails("Argument to GROUPING_ID operator must be a grouped expression");
     sql("select deptno, grouping_id(emp.^xxx^) from emp")
@@ -6213,18 +6218,120 @@ public class SqlValidatorTest extends SqlValidatorTestCase {
   }
 
   @Test public void testNestedAggOver() {
-    // windowed agg applied to agg is OK
-    check("select sum(max(empno))\n"
-        + "  OVER (order by deptno ROWS 2 PRECEDING)\n"
-        + "from emp");
-    check("select sum(max(empno)) OVER w\n"
-        + "from emp\n"
-        + "window w as (order by deptno ROWS 2 PRECEDING)");
+    sql("select sum(max(empno))\n"
+        + " OVER (order by ^deptno^ ROWS 2 PRECEDING)\n"
+        + " from emp")
+        .fails("Expression 'DEPTNO' is not being grouped");
+    sql("select sum(max(empno)) OVER w\n"
+        + " from emp\n"
+        + " window w as (order by ^deptno^ ROWS 2 PRECEDING)")
+        .fails("Expression 'DEPTNO' is not being grouped");
+    sql("select sum(max(empno)) OVER w2, sum(deptno) OVER w1\n"
+        + " from emp group by deptno\n"
+        + " window w1 as (partition by ^empno^ ROWS 2 PRECEDING),\n"
+        + " w2 as (order by deptno ROWS 2 PRECEDING)")
+        .fails("Expression 'EMP.EMPNO' is not being grouped");
+    sql("select avg(count(empno)) over w\n"
+        + "from emp group by deptno\n"
+        + "window w as (partition by deptno order by ^empno^)")
+        .fails("Expression 'EMPNO' is not being grouped");
 
     // in OVER clause with more than one level of nesting
     checkFails("select ^avg(sum(min(sal))) OVER (partition by deptno)^\n"
         + "from emp group by deptno",
         ERR_NESTED_AGG);
+
+    // OVER clause columns not appearing in GROUP BY clause NOT OK
+    sql("select avg(^sal^) OVER (), avg(count(empno)) OVER (partition by 1)\n"
+        + " from emp")
+        .fails("Expression 'SAL' is not being grouped");
+    sql("select avg(^sal^) OVER (), avg(count(empno)) OVER (partition by deptno)\n"
+        + " from emp group by deptno")
+        .fails("Expression 'SAL' is not being grouped");
+    sql("select avg(deptno) OVER (partition by ^empno^),\n"
+        + " avg(count(empno)) OVER (partition by deptno)\n"
+        + " from emp group by deptno")
+        .fails("Expression 'EMPNO' is not being grouped");
+    sql("select avg(deptno) OVER (order by ^empno^),\n"
+        + " avg(count(empno)) OVER (partition by deptno)\n"
+        + " from emp group by deptno")
+        .fails("Expression 'EMPNO' is not being grouped");
+    sql("select avg(sum(sal)) OVER (partition by ^empno^)\n"
+        + " from emp")
+        .fails("Expression 'EMPNO' is not being grouped");
+    sql("select avg(sum(sal)) OVER (partition by ^empno^)\n"
+        + "from emp group by deptno")
+        .fails("Expression 'EMPNO' is not being grouped");
+    sql("select avg(sum(sal)) OVER (partition by ^empno^)\n"
+        + " from emp")
+        .fails("Expression 'EMPNO' is not being grouped");
+    sql("select avg(sum(sal)) OVER (partition by ^empno^)\n"
+        + "from emp group by deptno")
+        .fails("Expression 'EMPNO' is not being grouped");
+    sql("select avg(sum(sal)) OVER (partition by deptno order by ^empno^)\n"
+        + "from emp group by deptno")
+        .fails("Expression 'EMPNO' is not being grouped");
+    sql("select avg(^sal^) OVER (),\n"
+        + " avg(count(empno)) OVER (partition by min(deptno))\n"
+        + " from emp")
+        .fails("Expression 'SAL' is not being grouped");
+    check("select avg(deptno) OVER (),\n"
+        + " avg(count(empno)) OVER (partition by deptno)"
+        + " from emp group by deptno");
+
+    // COUNT(*), PARTITION BY(CONST) with GROUP BY is OK
+    check("select avg(count(*)) OVER ()\n"
+        + " from emp group by deptno");
+    check("select count(*) OVER ()\n"
+        + " from emp group by deptno");
+    check("select count(deptno) OVER ()\n"
+        + " from emp group by deptno");
+    check("select count(deptno, deptno + 1) OVER ()\n"
+        + " from emp group by deptno");
+    sql("select count(deptno, deptno + 1, ^empno^) OVER ()\n"
+        + " from emp group by deptno")
+        .fails("Expression 'EMPNO' is not being grouped");
+    sql("select count(emp.^*^)\n"
+        + "from emp group by deptno")
+        .fails("Unknown field '\\*'");
+    sql("select count(emp.^*^) OVER ()\n"
+        + "from emp group by deptno")
+        .fails("Unknown field '\\*'");
+    check("select avg(count(*)) OVER (partition by 1)\n"
+        + " from emp group by deptno");
+    check("select avg(sum(sal)) OVER (partition by 1)\n"
+        + " from emp");
+    check("select avg(sal), avg(count(empno)) OVER (partition by 1)\n"
+        + " from emp");
+
+    // expression is OK
+    check("select avg(sum(sal)) OVER (partition by 10 - deptno\n"
+        + "   order by deptno / 2 desc)\n"
+        + "from emp group by deptno");
+    check("select avg(sal),\n"
+        + " avg(count(empno)) OVER (partition by min(deptno))\n"
+        + " from emp");
+    check("select avg(min(sal)) OVER (),\n"
+        + " avg(count(empno)) OVER (partition by min(deptno))\n"
+        + " from emp");
+
+    // expression involving non-GROUP column is not OK
+    sql("select avg(2+^sal^) OVER (partition by deptno)\n"
+            + "from emp group by deptno")
+            .fails("Expression 'SAL' is not being grouped");
+    sql("select avg(sum(sal)) OVER (partition by deptno + ^empno^)\n"
+        + "from emp group by deptno")
+        .fails("Expression 'EMPNO' is not being grouped");
+    check("select avg(sum(sal)) OVER (partition by empno + deptno)\n"
+        + "from emp group by empno + deptno");
+    check("select avg(sum(sal)) OVER (partition by empno + deptno + 1)\n"
+        + "from emp group by empno + deptno");
+    sql("select avg(sum(sal)) OVER (partition by ^deptno^ + 1)\n"
+        + "from emp group by empno + deptno")
+        .fails("Expression 'DEPTNO' is not being grouped");
+    check("select avg(empno + deptno) OVER (partition by empno + deptno + 1),\n"
+        + " count(empno + deptno) OVER (partition by empno + deptno + 1)\n"
+        + " from emp group by empno + deptno");
 
     // OVER in clause
     checkFails(